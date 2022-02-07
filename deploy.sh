abed4e733cccb69de83985c53272cdc75b584f11
Greg Robinson <g.robinson@f5.com>
2022-02-07 16:58:32 -0500

    creating terraform folder

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ rm -f $TF_LOG_PATH
 # init to ensure we have proper providers
 terraform init
 # run the plan to ensure we have proper configuration
-terraform plan -input=false -var-file=../admin.auto.tfvars -out tfplan
+terraform -chdir=terraform plan -input=false -var-file=../admin.auto.tfvars -out tfplan
 EXITCODE=$?
 # apply the plan if the planning operation was successful
-test $EXITCODE -eq 0 && terraform apply -input=false --auto-approve tfplan || echo "An error occurred while creating the Terraform plan"; 
+test $EXITCODE -eq 0 && terraform -chdir=terraform apply -input=false --auto-approve tfplan || echo "An error occurred while creating the Terraform plan"; 
 printf "$0 completed at $(date)"
